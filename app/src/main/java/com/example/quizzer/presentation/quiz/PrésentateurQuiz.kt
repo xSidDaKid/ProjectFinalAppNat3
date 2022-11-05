029dd13e55083bd027fe8210de2bf2ef2f855676
d1amondb0t <d1amondb0t@outlook.com>
2022-11-05 17:49:20 -0400

    Ajout du présentateurQuiz

---
+++ app/src/main/java/com/example/quizzer/presentation/quiz/PrésentateurQuiz.kt
@@ -0,0 +1,14 @@
+package com.example.quizzer.presentation.quiz
+
+import com.example.quizzer.presentation.quiz.IContratVuePrésentateurQuiz.*
+
+
+class PrésentateurQuiz : IPrésentateurQuiz{
+    override fun traiterRéponseSaisi(réponseChoisise: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun traiterQuitterQuiz() {
+        TODO("Not yet implemented")
+    }
+}
