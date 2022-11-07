643173888a5c8b3d5b2461362cf688c05b3ed2e7
d1amondb0t <d1amondb0t@outlook.com>
2022-11-07 10:46:22 -0500

    Ajout d'un modèle

---
+++ app/src/main/java/com/example/quizzer/presentation/Modèle.kt
@@ -0,0 +1,15 @@
+package com.example.quizzer.presentation
+
+import com.example.quizzer.domaine.entité.Quiz
+
+object Modèle {
+
+    private var Quiz = Quiz()
+
+    fun initialiserQuiz(){
+
+    }
+
+    fun getRéponse(){}
+
+}
