3cc8044964ad361be11b5b88e3a0cf51f6a7c6c4
d1amondb0t <d1amondb0t@outlook.com>
2022-11-17 21:47:11 -0500

    Modification de l'interaction verficationReponseCreationQuiz

--- app/src/main/java/com/example/quizzer/domaine/interacteur/VerificationReponseCreationQuiz.kt
+++ app/src/main/java/com/example/quizzer/domaine/interacteur/VerificationReponseCreationQuiz.kt
@@ -5,19 +5,27 @@ class VerificationReponseCreationQuiz {
     fun verificationReponseCreationQuiz(choixBrut: String, reponses: String) : Boolean {
         var choix = choixBrut.split(",")
         var premierFiltre = reponses.split(",")
-        var réponsesTriés: List<String> = emptyList()
-
-        for (item in premierFiltre) {
-            var deuxièmeFiltre = item.split(":")
-            réponsesTriés += deuxièmeFiltre[1]
-        }
+        var nbrChoix = 0
+        var réponsesTriés = obtenirReponsesSansChoix(premierFiltre)
         for (choi in choix){
-            for ( reponse in réponsesTriés){
-                if( reponse.lowercase() != choi.lowercase()){
-                    return false
-                }
+            nbrChoix++
+            if(!(réponsesTriés.contains(choi.lowercase()))){
+                return false
             }
+        }
+        if(nbrChoix != 4){
+            return false
         }
         return true
     }
+
+    fun obtenirReponsesSansChoix(reponses : List<String>): List<String>{
+        var réponsesTriés: List<String> = emptyList()
+        for (item in reponses) {
+            var deuxièmeFiltre = item.split(":")
+            réponsesTriés += deuxièmeFiltre[1].lowercase()
+        }
+        return réponsesTriés
+    }
+
 }
