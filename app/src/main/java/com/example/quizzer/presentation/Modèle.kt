b7a7df487b9a3a22172c24a94074bd6a85c9d5b7
d1amondb0t <d1amondb0t@outlook.com>
2022-11-28 13:48:20 -0500

    Click Fonctionnel

--- app/src/main/java/com/example/quizzer/presentation/Modèle.kt
+++ app/src/main/java/com/example/quizzer/presentation/Modèle.kt
@@ -16,10 +16,11 @@ import com.example.quizzer.domaine.interacteur.VerificationReponseCreationQuiz
 object Modèle {
 
     private var quizScore = QuizUtilisateurScore()
-    private var utilisateur = Utilisateur()
+    //private var utilisateur = Utilisateur()
 
     var tourDesRéponses: Int = 0
     var quizListe = mutableListOf<Quiz>()
+    var utilisateurListe = mutableListOf<Utilisateur>()
 
 
     init {
@@ -32,14 +33,14 @@ object Modèle {
      *
      */
     fun initialiserQuizParDefaut(): Quiz {
-        var newQuiz = Quiz(
+        /*var newQuiz = Quiz(
             0,
             "Les fruits et leurs couleurs",
             "Quelle est la couleur du fruit?",
             listOf<String>("Jaune", "Rouge", "Orange", "Vert"),
             ObtenirReponses().obtenirReponses(ReponsesParDefaut())
-        )
-        quizListe.add(newQuiz)
+        )*/
+        //quizListe.add(newQuiz)
         return quizListe[0]
     }
 
@@ -69,14 +70,35 @@ object Modèle {
     fun ajouterQuiz(
         titre: String,
         question: String,
-        choix: String,
-        reponse: String
+        choix: List<String>,
+        reponse: List<String>
     ) {
-        var reponseTrier = getReponseTrier(reponse)
-        var choixTrier : List<String> = choix.split(",")
-        var newQuiz = Quiz(1, titre, question, choixTrier, reponseTrier)
+        var compteur = 0
+        var reponseTrier: List<Map<String, String>> = emptyList()
 
+        while (compteur < choix.size) {
+            var reponseTrierChoix: List<String> =
+                ObtenirReponses().trierReponses2(reponse[compteur])
+            for (item in reponseTrierChoix) {
+                reponseTrier += mapOf(item to choix[compteur])
+            }
+            compteur++
+        }
+
+        var newQuiz = Quiz(1, titre, question, choix, reponseTrier)
         quizListe.add(newQuiz)
+    }
+
+    /**
+     * Méthode qui permet d'ajouter un nouveau utilisateur à la base de donnée
+     *
+     * @param email Email de l'utilisateur
+     * @param username Nom d'utilisateur de l'utilisateur
+     * @param mdp Mot de passe de l'utilisateur
+     */
+    fun ajouterUtilisateur(email: String, username: String, mdp: String) {
+        var newUtilisateur = Utilisateur(1, email, username, mdp)
+        utilisateurListe.add(newUtilisateur)
     }
 
     /**
@@ -85,9 +107,12 @@ object Modèle {
      * @param reponseBrut Réponse brut du créateur du quiz avec les , et :
      * @return
      */
-    fun getReponseTrier(reponseBrut: String): List<Map<String, String>> {
+    /*fun getReponseTrier(reponseBrut: String): List<Map<String, String>> {
         return ObtenirReponses().trierReponses(reponseBrut)
-    }
+    }*/
+    /* fun getReponseTrier(reponseBrut: List<String>): List<String> {
+         return ObtenirReponses().trierReponses(reponseBrut)
+     }*/
 
     /**
      * Méthode qui permet d'avoir la prochaine réponse
@@ -96,10 +121,14 @@ object Modèle {
      * @return
      */
     fun getProchaineRéponse(quiz: Quiz): String {
-        var réponse = quiz.reponses.get(tourDesRéponses)
-        tourDesRéponses++
-        var réponseString = réponse.toList().get(0).first
-        return réponseString
+        if (tourDesRéponses >= quiz.reponses.size)
+            return ""
+        else {
+            var réponse = quiz.reponses.get(tourDesRéponses)
+            tourDesRéponses++
+            var réponseString = réponse.toList().get(0).first
+            return réponseString
+        }
     }
 
     /**
@@ -145,6 +174,23 @@ object Modèle {
         return quizListe
     }
 
+    /**
+     * Méthode qui vérifie si l'utilisateur existe dans la BD
+     *
+     * @param nomUtilisateur Nom de l'utilisateur
+     * @param password Mot de passe de l'utilisateur
+     * @return True s'il existe; False s'il n'existe pas
+     */
+    fun getUilisateur(nomUtilisateur: String, password: String): Boolean {
+        for (item in utilisateurListe) {
+            if (item.nomUtilisateur == nomUtilisateur) {
+                if (item.motDePasse == password) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
 
     fun getRéponseParIndex(index: Int, quiz: Quiz): Map<String, String> {
         return quiz.reponses.get(index)
@@ -159,15 +205,17 @@ object Modèle {
     }
 
 
-    fun getnomUtilisateur(): String {
+    fun getnomUtilisateur(utilisateur: Utilisateur): String {
         return utilisateur.nomUtilisateur
     }
 
-    fun getPassword(): String {
+    fun getPassword(utilisateur: Utilisateur): String {
         return utilisateur.motDePasse
     }
 
     fun veriferQuiz(choix: String, reponse: String): String {
         return VerificationReponseCreationQuiz().verificationReponseCreationQuiz(choix, reponse)
     }
+
+
 }
--- app/src/main/java/com/example/quizzer/presentation/menuPrincipal/IContratVuePresentateurMenuPrincipal.kt
+++ app/src/main/java/com/example/quizzer/presentation/menuPrincipal/IContratVuePresentateurMenuPrincipal.kt
@@ -1,10 +1,16 @@
 package com.example.quizzer.presentation.menuPrincipal
 
+/**
+ * Interface qui permet à la vue de communiquer avec le présentateur et au présentateur de communiquer avec le modèle
+ * Vue <--> Présentateur ET Présentateur <--> Modèle
+ */
 interface IContratVuePresentateurMenuPrincipal {
     interface IVueMenuPrincipal {
         fun naviguerVersCreationQuiz()
         fun naviguerVersDemarrerQuiz()
         fun naviguerVersListeQuiz()
+        fun attacherÉcouteurCreerQuiz()
+        fun attacherÉcouteurDemarrerQuiz()
     }
 
     interface IPresentateurMenuPrincipal {
