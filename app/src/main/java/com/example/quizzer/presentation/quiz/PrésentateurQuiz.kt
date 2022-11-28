ed0272ac1194c20a2837fc8cf28fdca47db2256a
Shajaan Balasingam <shajaan_71@hotmail.com>
2022-11-27 23:34:33 -0500

    Ajout des commentaires pour le quiz

--- app/src/main/java/com/example/quizzer/presentation/quiz/PrésentateurQuiz.kt
+++ app/src/main/java/com/example/quizzer/presentation/quiz/PrésentateurQuiz.kt
@@ -2,7 +2,8 @@ package com.example.quizzer.presentation.quiz
 
 import com.example.quizzer.domaine.entité.Quiz
 import com.example.quizzer.presentation.Modèle
-import com.example.quizzer.presentation.quiz.IContratVuePrésentateurQuiz.*
+import com.example.quizzer.presentation.quiz.IContratVuePrésentateurQuiz.IPrésentateurQuiz
+import com.example.quizzer.presentation.quiz.IContratVuePrésentateurQuiz.IVueQuiz
 
 /**
  * Classe Présenteur qui permet de mettre à jour la vue Quiz
@@ -16,45 +17,76 @@ class PrésentateurQuiz(var modèle: Modèle, var vue: IVueQuiz = VueQuiz()) : I
         return modèle.getChoix()
     }*/
 
-    fun envoyerRéponse(réponse: String, indexRéponse: Int?,quiz:Quiz): Boolean {
+    /**
+     * Méthode qui permet d'envoyer la réponse choisie par l'utilisateur au modèle
+     *
+     * @param réponse Réponse
+     * @param indexRéponse Numéro de la réponse à trouver
+     * @param quiz Le quiz que l'utilisateur est entrain de faire
+     * @return True si c'est la bonne réponse; False si ce n'est pas la bonne réponse
+     */
+    override fun envoyerRéponse(réponse: String, indexRéponse: Int?, quiz: Quiz): Boolean {
         if (indexRéponse == null) {
-            return modèle.soumettreRéponse(réponse, 0,quiz)
+            return modèle.soumettreRéponse(réponse, 0, quiz)
         }
-        return modèle.soumettreRéponse(réponse, indexRéponse,quiz)
-
+        return modèle.soumettreRéponse(réponse, indexRéponse, quiz)
     }
 
-    fun envoyerProchaineRéponse(quiz:Quiz): String {
+    /**
+     * Méthode qui permet d'avoir la prochaine réponse
+     *
+     * @param quiz Le quiz que l'utilisateur est entrain de faire
+     * @return La prochaine réponse
+     */
+    override fun envoyerProchaineRéponse(quiz: Quiz): String {
         return modèle.getProchaineRéponse(quiz)
     }
 
-    fun envoyerIndexRéponse(): Int {
+    /**
+     * Méthode qui permet d'avoir l'index de la réponse où l'utilisateur est rendu
+     *
+     * @return L'index
+     */
+    override fun envoyerIndexRéponse(): Int {
         return modèle.getIndexRéponse()
     }
 
-
-    override fun traiterRéponseSaisi(réponseChoisise: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun traiterQuitterQuiz() {
-        TODO("Not yet implemented")
-    }
-
-
-   override fun réinitialiserQuiz(): Quiz {
+    /**
+     * Méthode qui permet d'initialiser un quiz
+     *
+     * @return Le quiz choisi
+     */
+    override fun réinitialiserQuiz(): Quiz {
         return modèle.initialiserQuizParDefaut()
     }
 
-    fun getQuestion(quiz:Quiz): String {
+    /**
+     * Méthode qui permet d'avoir la question du quiz
+     *
+     * @param quiz Le quiz choisi par l'utilisateur
+     * @return La question du quiz
+     */
+    override fun getQuestion(quiz: Quiz): String {
         return modèle.getQuestion(quiz)
     }
 
-    fun getTitre(quiz:Quiz): String {
+    /**
+     * Méthode qui permet d'avoir le titre du quiz
+     *
+     * @param quiz Le quiz choisi par l'utilisateur
+     * @return Le titre du quiz
+     */
+    override fun getTitre(quiz: Quiz): String {
         return modèle.getTitre(quiz)
     }
 
-    fun getScore(quiz:Quiz): Int {
+    /**
+     * Méthode qui permet d'avoir le score de l'utilisateur
+     *
+     * @param quiz Le quiz choisi par l'utilisateur
+     * @return Le nombre de bonne réponse
+     */
+    override fun getScore(quiz: Quiz): Int {
         return modèle.getScore(quiz)
     }
 }
