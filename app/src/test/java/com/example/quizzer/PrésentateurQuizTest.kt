6359395ffe361c069173eb5044ed8cbf32799e0e
d1amondb0t <d1amondb0t@outlook.com>
2022-12-13 18:30:47 -0500

    Continuation des tests unitaires

--- app/src/test/java/com/example/quizzer/PrésentateurQuizTest.kt
+++ app/src/test/java/com/example/quizzer/PrésentateurQuizTest.kt
@@ -8,6 +8,8 @@ import com.example.quizzer.presentation.quiz.PrésentateurQuiz
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.times
+import kotlin.text.Typography.times
 
 class PrésentateurQuizTest {
 
@@ -34,6 +36,17 @@ class PrésentateurQuizTest {
 
         assertEquals(résultat_attendu, résultat_obtenu)
 
+    }
+
+    @Test
+    fun `étant donné la dernière question dans un quiz choisi, lorsqu'on click un choix "Rouge", le score n'augmente pas et l'on continue à la vue de score `(){
+        val mockVue : IVueQuiz = Mockito.mock( IVueQuiz::class.java)
+        val présentateur = PrésentateurQuiz(mockVue)
+
+        Mockito.`when`( présentateur.envoyerRéponse("Rouge", 4, ReponsesParDefaut().quizSelectionné()) )
+            .thenReturn (true)
+
+        Mockito.verify( présentateur, times(1)).envoyerRéponse("Rouge", 4, ReponsesParDefaut().quizSelectionné())
 
     }
 }
