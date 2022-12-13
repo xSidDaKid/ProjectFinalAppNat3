b1a6ceaa174c7f1d8e8feea0876c79c73cababbd
d1amondb0t <d1amondb0t@outlook.com>
2022-12-13 18:12:52 -0500

    Commencement des tests unitaires

--- app/src/test/java/com/example/quizzer/PrésentateurQuizTest.kt
+++ app/src/test/java/com/example/quizzer/PrésentateurQuizTest.kt
@@ -1,4 +1,39 @@
 package com.example.quizzer
 
+import com.example.quizzer.accesAuxDonnées.ReponsesParDefaut
+import com.example.quizzer.presentation.Modèle
+import com.example.quizzer.presentation.modèle
+import com.example.quizzer.presentation.quiz.IContratVuePrésentateurQuiz.IVueQuiz
+import com.example.quizzer.presentation.quiz.PrésentateurQuiz
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito
+
 class PrésentateurQuizTest {
+
+    @Test
+    fun `étant donné une vue avec un quiz choisi, lorsqu'on click un choix "Rouge", le score augmente de 1 et l'on continue à la prochaine question `(){
+
+        val mockVue : IVueQuiz = Mockito.mock( IVueQuiz::class.java)
+        val présentateur = PrésentateurQuiz(mockVue)
+
+        val résultat_obtenu = présentateur.envoyerRéponse("Rouge", 1, ReponsesParDefaut().quizSelectionné())
+        val résultat_attendu = true
+
+        assertEquals(résultat_attendu, résultat_obtenu)
+
+    }
+
+    @Test
+    fun `étant donné la deuxième question dans un quiz choisi, lorsqu'on click un choix "Rouge", le score n'augmente pas et l'on continue à la prochaine question `(){
+        val mockVue : IVueQuiz = Mockito.mock( IVueQuiz::class.java)
+        val présentateur = PrésentateurQuiz(mockVue)
+
+        val résultat_obtenu = présentateur.envoyerRéponse("Rouge", 2, ReponsesParDefaut().quizSelectionné())
+        val résultat_attendu = false
+
+        assertEquals(résultat_attendu, résultat_obtenu)
+
+
+    }
 }
